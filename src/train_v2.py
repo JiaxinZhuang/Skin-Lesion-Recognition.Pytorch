d08d944fd51a42e467ac112a00f80626d1d9fce4
lincolnz9511 <lincolnz9511@gmail.com>
2018-06-23 17:25:32 +0800

    fix

--- src/train_v2.py
+++ src/train_v2.py
@@ -1,9 +1,10 @@
-from keras.preprocessing import image
-from keras.applications import resnet50
-from keras.models import Model
-from keras.layers import Dense, GlobalAveragePooling2D, Input
-from keras.optimizers import SGD
-from keras import losses
+import tensorflow as tf
+from tf.keras.preprocessing import image
+from tf.keras.applications import resnet50
+from tf.keras.models import Model
+from tf.keras.layers import Dense, GlobalAveragePooling2D, Input
+from tf.keras.optimizers import SGD
+from tf.keras import losses
 import numpy as np
 
 batch_size = 32
@@ -21,17 +22,14 @@ model = Model(inputs=base_model.input, outputs=predictions)
 for layer in base_model.layers:
     layer.trainable = False
 
-    model.compile(loss='categorical_crossentropy',
-                        optimizer=SGD(lr=0.01, momentum=0.9, decay=0.0),
-                        metrics=['categorical_accuracy', 'acc'])
-
-    x_train = np.random.normal(loc=127, scale=127, size=(50, 224,224,3))
-    y_train = np.array([0,1]*25)
-    x_train = resnet50.preprocess_input(x_train)
+model.compile(loss='categorical_crossentropy',
+              optimizer=SGD(lr=0.01, momentum=0.9, decay=0.0),
+              metrics=['acc', 'categorical_accuracy'])
+x_train = resnet50.preprocess_input(x_train)
 
     print(model.evaluate(x_train, y_train, batch_size=batch_size, verbose=0))
         model.fit(x_train, y_train,
                           epochs=100,
                                     batch_size=batch_size,
                                               shuffle=False,
-                                                        validation_data=(x_train, y_train))
+                                                        validation_dat=(x_train, y_train))
